2ebd895592f1984b1554c50341964fa81ee1472c
izaazm <mizaazir2@gmail.com>
2024-05-22 16:21:00 +0900

    change eval of qual

--- dataset.py
+++ dataset.py
@@ -303,11 +303,12 @@ class TestNewData():
 		filter_dict = {}
 		for triplet in total_triplets:
 			h, r, t = triplet
-			if ('_', self.rel2id[r], self.ent2id[t]) not in filter_dict:
-				filter_dict[('_', self.rel2id[r], self.ent2id[t])] = [self.ent2id[h]]
-			else:
-				filter_dict[('_', self.rel2id[r], self.ent2id[t])].append(self.ent2id[h])
-
+			if not self.qual:
+				if ('_', self.rel2id[r], self.ent2id[t]) not in filter_dict:
+					filter_dict[('_', self.rel2id[r], self.ent2id[t])] = [self.ent2id[h]]
+				else:
+					filter_dict[('_', self.rel2id[r], self.ent2id[t])].append(self.ent2id[h])
+			
 			if (self.ent2id[h], '_', self.ent2id[t]) not in filter_dict:
 				filter_dict[(self.ent2id[h], '_', self.ent2id[t])] = [self.rel2id[r]]
 			else:
