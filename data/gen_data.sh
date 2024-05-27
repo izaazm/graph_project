a613428832589e330522be967ebde2626d329c46
izaazm <mizaazir2@gmail.com>
2024-05-28 01:59:47 +0900

    changed script

--- data/gen_data.sh
+++ data/gen_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python3 generate_data.py ./JF17K/full $1 120 160 0.5 0.5
+python3 generate_data.py ./JF17K/full $1 90 120 0.5 0.2
 python3 val_test_triplet.py $1
 python3 val_test_qual.py $1
 python3 check.py $1
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+
 python3 test.py --data_path ./data/ --data_name $1 --num_epoch 8000
